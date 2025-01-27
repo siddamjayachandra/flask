18b6b2474ad7dbb64a54d1b3fd80c8e28a47ae20
root <root@ip-172-31-32-137.ap-south-1.compute.internal>
2025-01-27 13:54:07 +0000

    printing nothing

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, It is not working as expected'
+    return 'Hello, It is not working as expected what to do next'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
