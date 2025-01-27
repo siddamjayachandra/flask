0c9566b5118336337a9fec29d1355ab8089b56a6
root <root@ip-172-31-32-137.ap-south-1.compute.internal>
2025-01-27 13:46:04 +0000

    printing time and date

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Today is monday onjanuary 27th of 2029'
+    # Get the current date and time
+    current_datetime = datetime.now().strftime("%A, %B %d, %Y %I:%M:%S %p")
+    return f"Hello, Today is {current_datetime}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
