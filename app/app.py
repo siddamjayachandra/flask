ce5fb1a099171fd749829aa65b499e68d0c66cfd
root <root@ip-172-31-32-137.ap-south-1.compute.internal>
2025-01-27 12:16:27 +0000

    year change

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Today is monday onjanuary 27th of 2025'
+    return 'Hello, Today is monday onjanuary 27th of 2029'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
